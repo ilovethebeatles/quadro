c6bbd289ea193145f7a3cad36bc3bb809b53d49c
ilovethebeatles <106533857+ilovethebeatles@users.noreply.github.com>
2022-09-16 20:53:30 +0300

    Update quadratic.cpp

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,6 +1,9 @@
 #include "quadratic.h"
 
 double BiggerRoot(const double a, const double b, const double c) {
+  if (a == 0) {
+    double bigger_root = - c / b;
+  }
   double d = b * b - 4 * a * c;
   double bigger_root = (- b + sqrt(d)) / (2 * a);
   return bigger_root;
@@ -20,17 +23,22 @@ short int NumOfRoots(const double a, const double b, const double c) {
   if (d == 0 || a == 0) {
     return 1;
   }
-  return 2;
+  if (d > 0) {
+    return 2;
+  }
+  return -1;
 }
 
 void PrintRoots (const double a, const double b, const double c) {
     short int num = NumOfRoots(a, b, c);
     if (num == 0) {
-      printf("No roots!");
+      printf("No roots!\n");
     } else if (num == 1) {
-      printf("%.2lf is a root/n", BiggerRoot(a, b, c));
+      printf("%.2lf is a root\n", BiggerRoot(a, b, c));
+    } else if (num = -1) {
+      printf("Infinite number of roots\n");
     } else {
-      printf("%.2lf is a smaller root/n", SmallerRoot(a, b, c));
-      printf("%.2lf is a bigger root/n", BiggerRoot(a, b, c));
+      printf("%.2lf is a smaller root\n", SmallerRoot(a, b, c));
+      printf("%.2lf is a bigger root\n", BiggerRoot(a, b, c));
     }
 }
